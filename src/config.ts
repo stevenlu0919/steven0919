37c68c86e16835d03c19486d37b6cb3bed7e4684
steven <770289767@qq.com>
2025-05-26 18:09:54 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -12,12 +12,12 @@ export const siteConfig: SiteConfig = {
     lang: "zh_CN", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
     themeColor: {
         hue: 280, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
-        fixed: false, // Hide the theme color picker for visitors
+        fixed: ture, // Hide the theme color picker for visitors
     },
     banner: {
         enable: false,
         src: "assets/images/demo-banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-        position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
+        position: "bottom", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
         credit: {
             enable: false, // Display the credit text of the banner image
             text: "", // Credit text to be displayed
