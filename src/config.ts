1ed2f82f56e06f3b99f9fe42a6ab4c2f5b7927b7
steven <770289767@qq.com>
2025-05-26 18:12:54 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -12,7 +12,7 @@ export const siteConfig: SiteConfig = {
     lang: "zh_CN", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
     themeColor: {
         hue: 280, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
-        fixed: ture, // Hide the theme color picker for visitors
+        fixed: true, // Hide the theme color picker for visitors
     },
     banner: {
         enable: false,
